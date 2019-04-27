b0e9843c7c65e694456cfa851d85a72dec950200
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-27 21:58:06 +0200

    Add price discount, Add ternary

--- engine.js
+++ engine.js
@@ -11,6 +11,9 @@ module.exports = function (app) {
             arr: function (arguments) {
                 return arguments.split(',');
             },
+            ternary: function (arg1, arg2) {
+                return arg1 ?  arg1 : arg2;
+            },
             equals: function (arg1, arg2, options) {
                 return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
             },
