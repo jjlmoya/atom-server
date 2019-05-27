ce8d0cd94ef2e9589bf577737d5438c6fc7f304e
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-05-27 18:30:59 +0200

    Use mocks

--- settings/mock.js
+++ settings/mock.js
@@ -3,6 +3,7 @@ module.exports = {
         name: 'Producto',
         image: 'https://d2wnixsywa9nb4.cloudfront.net/wp-content/uploads/2017/12/02135818/LeetCode_logo.png',
         description: 'Una pequeña descripción de producto',
+        subtitle: 'Test',
         transparent: 'https://cryptorunner.com/wp-content/uploads/2017/10/ethereum-symbol.png',
         price: 35,
         currency: '€',
@@ -14,6 +15,7 @@ module.exports = {
             image: 'https://cdn-images-1.medium.com/max/1200/1*SlXjjZ8ghJI9w37gzpG9FA.png',
             transparent: 'https://cryptorunner.com/wp-content/uploads/2017/10/ethereum-symbol.png',
             description: 'Una pequeña descripción de producto',
+            subtitle: 'Test',
             price: 25,
             currency: '€',
             link: ''
@@ -23,6 +25,7 @@ module.exports = {
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png',
             transparent: 'https://cryptorunner.com/wp-content/uploads/2017/10/ethereum-symbol.png',
             description: 'Una pequeña descripción de producto',
+            subtitle: 'Test',
             price: 10,
             currency: '€',
             link: ''
