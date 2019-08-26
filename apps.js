85e289c24dcaa1a1ca20a3990034e94394cd9b1f
jjlmoya <juanjo1989@gmail.com>
2019-08-26 21:18:37 +0200

    Remove Mongo Dependency

--- apps.js
+++ apps.js
@@ -3,23 +3,13 @@ const port = Config.server().port;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const db = require('./db');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-db.connect(Config.db().protocol + Config.db().url + Config.db().dbName, function (err) {
-    if (err) {
-        console.log('Unable to connect to Mongo.');
-        process.exit(1);
-    } else {
-        app.listen(port, function () {
-            console.log('Example app listening on port ' + port);
-        });
-    }
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
 });
-
 require('./engine')(app);
 require('./router')(app);
 
